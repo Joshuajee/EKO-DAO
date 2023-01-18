f13699dca69a4da7970f6035ae6ee15af7fa64e1
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2023-01-18 20:56:56 +0100

    feat: little nav

--- dao-frontend/src/components/dashboard/TopNav.jsx
+++ dao-frontend/src/components/dashboard/TopNav.jsx
@@ -1,7 +1,10 @@
 const TopNav = () => {
     return (
-        <nav style={{height: "70px"}} className="w-full border-b-2 border-gray-200">
+        <nav style={{height: "70px"}} className="px-20 py-4 flex justify-between w-full border-b-2 border-gray-200">
+            
+            <h1 className="text-2xl text-blue-800 font-bold">EkoDAO</h1>
 
+            <button className="px-10 rounded-lg h-10 text-md bg-blue-800 text-white hover:bg-blue-900">Connect Wallet </button>
         </nav>
     )
 }
