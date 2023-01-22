e6241b0ed1f43e76e38d00c9a94374d0725f3835
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2023-01-22 22:04:31 +0100

    Feat: Designed dashboard

--- dao-frontend/__test__/pages/index.test.js
+++ dao-frontend/__test__/pages/index.test.js
@@ -1,7 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import user from "@testing-library/user-event";
 import Home from '../../src/pages/';
 
-test('renders component', () => {
-  render(<Home />);
+describe ("Testing the landing page", () => {
 
-});
+  it('Explore Button is present, and leads to dashboard when clicked', async() => {
+    
+    render(<Home />);
+  
+    const exploreBtn = screen.getByRole("button", {
+      name: /explore/i
+    })
+  
+    expect(exploreBtn).toBeInTheDocument()
+
+  });
+
+})
+
+
