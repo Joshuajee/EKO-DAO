c4fa1381b9fe4e2175dad4c3d16cc5be91136116
trysam2003 <trysam2003@gmail.com>
2023-02-15 22:29:38 +0100

    Merge branch 'dev' of https://github.com/Joshuajee/EKO-DAO into dev

--- dao-protocol/contracts/facets/CrowdFundFacet.sol
+++ dao-protocol/contracts/facets/CrowdFundFacet.sol
@@ -5,35 +5,36 @@ import '../libraries/LibCrowdFund.sol';
 import "../CrowdFundProject.sol";
 
 contract CrowdFundFacet{
-      event ProjectStarted(
-        address projectContractAddress,
-        string title,
-        string desc,
-        uint256 targetFund,               
-        uint256 minContribution,
-        uint256 startDate,
-        uint256 endDate
-    );
+  event ProjectStarted(
+    address projectContractAddress,
+    string title,
+    string desc,
+    uint256 targetFund,
+    uint256 minContribution,
+    uint256 startDate,
+    uint256 endDate
+  );
 
-    event ContributionReceived(
-        address projectAddress,
-        uint256 contributedAmount,
-        address indexed contributor
-    );
+  event ContributionReceived(
+    address projectAddress,
+    uint256 contributedAmount,
+    address indexed contributor
+  );
 
-    event FundWithdrawn(
-        address projectAddress,
-        uint256 amountWithdrawn,
-        address indexed withdrawer
-    );
+  event FundWithdrawn(
+    address projectAddress,
+    uint256 amountWithdrawn,
+    address indexed withdrawer
+  );
 
 
     error ProjectDoesNotExist(uint256 projectIndex);
-    
 
-    // @dev Anyone can start a fund rising
-    // @return null
-    
+  
+
+  // @dev Anyone can start a fund rising
+// @return null
+
     function createCampaign(
         string  memory _projectTopic,
         string  memory _description,
@@ -49,53 +50,53 @@ contract CrowdFundFacet{
     }
 
 
-// Donate acceptable stable coin by anyone
-function donate(uint256 _projectIndex, uint256 _tokenAmount) public {
+  // Donate acceptable stable coin by anyone
+  function donate(uint256 _projectIndex, uint256 _tokenAmount) public {
     address _user = msg.sender;
 
     Project projectAddress = Database.getCrowdFundMappingRecords().Projects[_projectIndex];
     if(address(projectAddress) == address(0)){
-        revert ProjectDoesNotExist(_projectIndex);
+      revert ProjectDoesNotExist(_projectIndex);
     }
-   
-    Project(projectAddress).donate(_user, _tokenAmount); // Call function   
-    //emit ContributionReceived(address(projectAddress),_tokenAmount,_user);// Trigger event 
-}
 
-// Admin withdraw if project is successful 
+    Project(projectAddress).donate(_user, _tokenAmount); // Call function
+    //emit ContributionReceived(address(projectAddress),_tokenAmount,_user);// Trigger event
+  }
 
-function adminWithdraw(uint256 _projectIndex, uint256 _tokenAmount) public {
+  // Admin withdraw if project is successful
+
+  function adminWithdraw(uint256 _projectIndex, uint256 _tokenAmount) public {
     address _user = msg.sender;
 
     Project projectAddress = Database.getCrowdFundMappingRecords().Projects[_projectIndex];
     if(address(projectAddress) == address(0)){
-        revert ProjectDoesNotExist(_projectIndex);
+      revert ProjectDoesNotExist(_projectIndex);
     }
-   
-    Project(projectAddress).adminWithdraw(_user, _tokenAmount); // Call function   
+
+    Project(projectAddress).adminWithdraw(_user, _tokenAmount); // Call function
     emit FundWithdrawn(address(projectAddress),_tokenAmount,_user);// Trigger event 
-}
+  }
 
 
-// Donor withdraw all donated amount if project is failed
+  // Donor withdraw all donated amount if project is failed
 
-function donorWithdraw(uint256 _projectIndex) public {
+  function donorWithdraw(uint256 _projectIndex) public {
     address _user = msg.sender;
 
     Project projectAddress = Database.getCrowdFundMappingRecords().Projects[_projectIndex];
     if(address(projectAddress) == address(0)){
-        revert ProjectDoesNotExist(_projectIndex);
+      revert ProjectDoesNotExist(_projectIndex);
     }
 
     uint256 amountDonated = Project(projectAddress).getAmountDonated(_user);
-   
-    Project(projectAddress).donorWithdraw(_user); // Call function   
+
+    Project(projectAddress).donorWithdraw(_user); // Call function
     emit FundWithdrawn(address(projectAddress),amountDonated,_user);// Trigger event
-}
+  }
 
 // Get number of projects created
 function returnProjectsCount() external view returns(uint256){
-    return  Database.getCrowdFundRecords().projectCounts;
+   return  Database.getCrowdFundRecords().projectCounts;
 }
 
 //Get the details of the last 5 Projects ------- work in progress ------
@@ -128,18 +129,18 @@ function getLastXProjectDetails(uint X) public view returns(
 }
 
 
-//Get project token balance
+  //Get project token balance
 function getProjectBalance(uint256 _projectIndex) public view returns(uint256) {
     Project projectAddress = Database.getCrowdFundMappingRecords().Projects[_projectIndex];   
     return Project(projectAddress).getProjectBalance();  // Call function
-}
+  }
 
 
-//Get project details
+  //Get project details
 function getProjectDetails(uint256 _projectIndex) public view returns(Database.ProjectState memory) {
     address projectAddress = address(Database.getCrowdFundMappingRecords().Projects[_projectIndex]);   
     return Project(projectAddress).getProjectDetails();  // Call function
-}
+  }
 
 
 
