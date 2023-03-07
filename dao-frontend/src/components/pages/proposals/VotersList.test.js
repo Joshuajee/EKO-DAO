f7d72cb49254de1e19628d0d93eea4bfbd872195
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2023-03-07 14:53:13 +0100

    fix: clean up

--- dao-frontend/src/components/pages/proposals/VotersList.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import VotersList from './VotersList';
-
-test('renders component', () => {
-  render(<VotersList />);
-
-});
